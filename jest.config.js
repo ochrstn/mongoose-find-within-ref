1cefb5bee98f033878efee304d657d1ba55d1bef
Oliver Christen <christen@app-logik.de>
2024-04-02 12:08:34 +0200

    update Jest testRegex pattern

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.tsx?$",
+  testRegex: "(test|spec)\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
